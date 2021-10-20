39af34c6fedc358e57d5426793fdb8588cb9c90b
Theo <tstavrik@gmail.com>
2021-10-20 20:09:19 +0100

    health mechanics

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer; 
+    public float speed = 3.0f;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
     int currentHealth;
 
     Rigidbody2D rb;
@@ -23,16 +28,32 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime; 
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
+
     void FixedUpdate()
     {
         Vector2 position = rb.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         rb.MovePosition(position);
     }
-    void ChangeHealth(int amount)
-    {
+
+    public void ChangeHealth(int amount)
+    {   if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
